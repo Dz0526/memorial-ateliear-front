cfd7d490ef549178a34675370266e4227387f58d
lemonadern <taishi.develop@gmail.com>
2024-03-13 12:47:33 +0900

    add link to feed, to home icon

--- src/shared/components/layouts/BottomMenu.tsx
+++ src/shared/components/layouts/BottomMenu.tsx
@@ -4,6 +4,7 @@ import { BiPhotoAlbum } from 'react-icons/bi';
 import { MdOutlineAddBox } from 'react-icons/md';
 import { IoSearch } from 'react-icons/io5';
 import { FiHome } from 'react-icons/fi';
+import Link from 'next/link';
 
 export const BottomMenu = () => {
   return (
@@ -15,7 +16,9 @@ export const BottomMenu = () => {
       borderColor={'gray.300'}
       bg={'white'}
     >
-      <Icon as={FiHome} boxSize={'32px'} />
+      <Link href='feed' passHref>
+        <Icon as={FiHome} boxSize={'32px'} />
+      </Link>
       <Icon as={IoSearch} boxSize={'32px'} />
       <Icon as={MdOutlineAddBox} boxSize={'32px'} />
       <Icon as={BiPhotoAlbum} boxSize={'32px'} />
