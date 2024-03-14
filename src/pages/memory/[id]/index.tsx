18a3c9a249159a13046b3d1be77e68896a59b98e
lemonadern <taishi.develop@gmail.com>
2024-03-14 22:58:25 +0900

    implement memory page

--- src/pages/memory/[id]/index.tsx
+++ src/pages/memory/[id]/index.tsx
@@ -2,12 +2,75 @@ import React from 'react'
 import { Layout } from 'shared/components/layouts/Layout';
 import type * as next from 'next';
 import { BackAndKebabHeader } from 'shared/components/layouts/BackAndKebabHeader';
+import { Text, Flex, HStack, Image, Heading, Stack, Button } from '@chakra-ui/react';
+import { FaArrowRight } from "react-icons/fa6";
+import { UserCardBanner } from 'shared/components/feed/UserCardBanner';
+
+const mockedMemory = {
+  id: 'sampleMemoryId',
+  imageUrl: 'https://i.postimg.cc/g0ffYX8d/IMG-6315.jpg',
+  caption: 'キャンプの思い出',
+  memoryTimeContext: '2024年3月',
+  members: [
+    {
+      id: '1',
+      screenName: 'Daiki Ito',
+      iconImageUrl: '',
+    },
+    {
+      id: '2',
+      screenName: 'Manato Kato',
+      iconImageUrl: '',
+    },
+    {
+      id: '3',
+      screenName: 'Taishi Naka',
+      iconImageUrl: '',
+    },
+  ],
+}
+
 
 const Memory: next.NextPageWithLayout = () => {
   return (
     <>
       <BackAndKebabHeader backHref='/feed' kebabHref='edit' />
-      <div> Memory</ div>
+      {/* Imamge */}
+      <Flex justify="center">
+        <Image
+          src={mockedMemory.imageUrl}
+          alt='memory'
+          maxHeight={'70vh'}
+        />
+      </Flex>
+
+      {/* 詳細情報 */}
+      <Stack spacing={'8'} paddingX={'4'} marginTop={'4'}>
+
+        {/* caption and when */}
+        <Stack>
+          <Heading size={'lg'}>{mockedMemory.caption}</Heading>
+          <HStack alignItems={'end'} spacing={'1'}>
+            <Text fontSize={'sm'} fontWeight={'bold'}>{mockedMemory.memoryTimeContext}</Text>
+            <Text fontSize={'sm'}>の思い出</Text>
+          </HStack>
+        </Stack>
+
+        {/* members */}
+        <Stack spacing={4}>
+          <Heading size={'sm'}>思い出のメンバー</Heading>
+          <Stack spacing={4} paddingLeft={2}>
+            {mockedMemory.members.map((member) => (
+              <UserCardBanner key={member.id} {...member} />
+            ))}
+          </Stack>
+        </Stack>
+
+        <Button variant={'outline'} alignSelf={'center'} rightIcon={<FaArrowRight />}>
+          思い出からクエストを作成する
+        </Button>
+      </Stack>
+
     </>
   );
 }
