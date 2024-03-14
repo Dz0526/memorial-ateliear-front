410bc8da1d28397278eaae1f34897cd1ee12de72
lemonadern <taishi.develop@gmail.com>
2024-03-14 23:11:24 +0900

    update mock data

--- src/shared/components/feed/ChekiProps.ts
+++ src/shared/components/feed/ChekiProps.ts
@@ -19,9 +19,9 @@
 
 export const mockedChekiProps = {
   memoryId: 'sampleMemoryId',
-  imageUrl: 'https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
-  caption: 'ふかふかソファに座ってたのしい談笑',
-  memoryTimeContext: '寮生活時代',
+  imageUrl: 'https://i.postimg.cc/g0ffYX8d/IMG-6315.jpg',
+  caption: 'キャンプの思い出',
+  memoryTimeContext: '2024年3月',
   members: mockedMembers,
 };
 
