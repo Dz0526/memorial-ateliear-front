b14666d54641ac2dacb128e51cba1d53de084a04
lemonadern <taishi.develop@gmail.com>
2024-03-23 08:50:28 +0900

    extract ConnectModal

--- src/pages/profile/[profileId]/index.tsx
+++ src/pages/profile/[profileId]/index.tsx
@@ -22,10 +22,7 @@ type ProfileResponse = {
 
 const Profile: next.NextPageWithLayout = () => {
   const router = useRouter();
-  const [username, setUsername] = useState('');
 
-  const [successMessage, setSuccessMessage] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
 
   const { data, isLoading, error, refetch } = useQuery<ProfileResponse, AxiosError>({
     queryKey: ['profile', router.query.profileId],
@@ -35,6 +32,94 @@ const Profile: next.NextPageWithLayout = () => {
         .then(res => res.data),
 
   });
+
+
+
+  const isLinked = data && data.linkedUser && data.linkedUser.username;
+
+  const { isOpen: isConnectModalOpen, onOpen: onConnectModalOpen, onClose: onConnectModalClose } = useDisclosure()
+
+
+  return (
+    <>
+
+      <Box paddingBottom={'40'}>
+
+        {/* header menu */}
+        <BackAndKebabHeader>
+          <MenuList>
+            <MenuItem>
+              <Link width={'100%'} height={'100%'} href={`${router.query.profileId}/edit`}>
+                プロフィールを編集する
+              </Link>
+            </MenuItem>
+            <MenuItem> プロフィールを削除（no implementation）</MenuItem>
+          </MenuList>
+        </BackAndKebabHeader>
+
+        {/* loading and error on query */}
+        {
+          isLoading &&
+          <Center minHeight={'50vh'}>
+            <Spinner size='xl' />
+          </Center>
+
+        }
+        {
+          error && <Text color={'red'}>Error: {error.message}</Text>
+        }
+
+        {/* display query data */}
+        {data && (
+          <Stack spacing={8}>
+
+            {/* icon and name */}
+            <VStack paddingTop={8} spacing={'4'}>
+              <Avatar src={`${process.env.NEXT_PUBLIC_STORAGE_ORIGIN}${data.iconUrl}`} name={data.screenName} boxSize={'100px'} />
+              <Heading size="md">{data.screenName}</Heading>
+            </VStack>
+
+            <Stack spacing={4}>
+              {/* linked user status */}
+              {
+                isLinked ? (
+                  <HStack justifyContent={'center'}>
+                    <FaUserCheck color='green' size={16} />
+                    <Text fontWeight={'semibold'}>{data.linkedUser.username}</Text>
+                  </HStack>
+                ) : (
+                  <HStack justifyContent={'center'}>
+                    <Button leftIcon={<FaUserPlus color='gray' size={16} />} size={'sm'} onClick={onConnectModalOpen}>ユーザと紐付ける</Button>
+                  </HStack>
+
+                )
+              }
+            </Stack>
+
+            {/* memo */}
+            <Stack spacing={2} paddingX={8} alignSelf={'center'}>
+              <Heading size={'sm'} alignSelf={'center'}>メモ</Heading>
+              <Text fontSize={'sm'}>{data.memo}</Text>
+            </Stack>
+
+          </Stack>
+        )}
+      </Box>
+      <ConnectModal data={data} isOpen={isConnectModalOpen} onClose={() => { refetch(); onConnectModalClose(); }} />
+
+
+
+    </>
+  );
+}
+
+const ConnectModal = (
+  { data, isOpen, onClose }: { data: ProfileResponse | undefined, isOpen: boolean, onClose: () => void }
+) => {
+  const [username, setUsername] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
 
   const { mutate } = useMutation({
     mutationFn: (input: { linkUserUsername: string; }) =>
@@ -56,110 +141,42 @@ const Profile: next.NextPageWithLayout = () => {
     },
   });
 
-  const isLinked = data && data.linkedUser && data.linkedUser.username;
-  const { isOpen, onOpen, onClose } = useDisclosure()
-
   return (
-    <>
+    <Modal isOpen={isOpen} onClose={onClose} size={"xs"} >
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>ユーザへの紐づけ</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <VStack spacing={'8'}>
 
-      <Box paddingBottom={'40'}>
-        <BackAndKebabHeader>
-          <MenuList>
-            <MenuItem>
-              <Link width={'100%'} height={'100%'} href={`${router.query.profileId}/edit`}>
-                プロフィールを編集する
-              </Link>
-            </MenuItem>
-            <MenuItem> プロフィールを削除（no implementation）</MenuItem>
-          </MenuList>
-        </BackAndKebabHeader>
-        {
-          isLoading &&
-          <Center minHeight={'50vh'}>
-            <Spinner size='xl' />
-          </Center>
-
-        }
-        {
-          error && <Text color={'red'}>Error: {error.message}</Text>
-        }
-        {data && (
-          <Stack spacing={8}>
-
-            {/* icon and name */}
-            <VStack paddingTop={8} spacing={'4'}>
-              <Avatar src={`${process.env.NEXT_PUBLIC_STORAGE_ORIGIN}${data.iconUrl}`} name={data.screenName} boxSize={'100px'} />
-              <Heading size="md">{data.screenName}</Heading>
+            <VStack>
+              <Avatar src={`${process.env.NEXT_PUBLIC_STORAGE_ORIGIN}${data?.iconUrl}`} name={data?.screenName} boxSize={'100px'} />
+              <Heading size="md">{data?.screenName}</Heading>
             </VStack>
 
-            <Stack spacing={4}>
-              {/* linked user status */}
-              {
-                isLinked ? (
-                  <HStack justifyContent={'center'}>
-                    <FaUserCheck color='green' size={16} />
-                    <Text fontWeight={'semibold'}>{data.linkedUser.username}</Text>
-                  </HStack>
-                ) : (
-                  <HStack justifyContent={'center'}>
-                    <Button leftIcon={<FaUserPlus color='gray' size={16} />} size={'sm'} onClick={onOpen}>ユーザと紐付ける</Button>
-                  </HStack>
-
-                )
-              }
-
-              {/* edit button */}
-              <HStack justifyContent={'center'}>
-                <Button size={'sm'}>プロフィールを編集</Button>
-              </HStack>
-            </Stack>
-
-          </Stack>
-        )}
-      </Box>
-
-
-      <Modal isOpen={isOpen} onClose={
-        () => {
-          refetch();
-          onClose();
-        }
-      } size={"xs"}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>ユーザへの紐づけ</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <VStack spacing={'8'}>
-
-              <VStack>
-                <Avatar src={`${process.env.NEXT_PUBLIC_STORAGE_ORIGIN}${data?.iconUrl}`} name={data?.screenName} boxSize={'100px'} />
-                <Heading size="md">{data?.screenName}</Heading>
-              </VStack>
-
-              <VStack>
-                <FormLabel htmlFor="username">紐付けるユーザ名</FormLabel>
-                <Input id="username" onChange={(e) => { setUsername(e.target.value) }} />
-                <Button onClick={() => {
-                  mutate({ linkUserUsername: username });
-                }}>つなげる</Button>
-              </VStack>
+            <VStack>
+              <FormLabel htmlFor="username">紐付けるユーザ名</FormLabel>
+              <Input id="username" onChange={(e) => { setUsername(e.target.value) }} />
+              <Button onClick={() => {
+                mutate({ linkUserUsername: username });
+              }}>つなげる</Button>
             </VStack>
-          </ModalBody>
+          </VStack>
+        </ModalBody>
 
-          <ModalFooter>
-            <Text hidden>紐付けるユーザ名を入力し、「つなげる」を押してください</Text>
-            {
-              errorMessage !== '' && <Text color={'red.400'}>{errorMessage}</Text>
-            }
-            {
-              successMessage !== '' && <Text color={'green'}>{successMessage}</Text>
-            }
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    </>
-  );
+        <ModalFooter>
+          <Text hidden>紐付けるユーザ名を入力し、「つなげる」を押してください</Text>
+          {
+            errorMessage !== '' && <Text color={'red.400'}>{errorMessage}</Text>
+          }
+          {
+            successMessage !== '' && <Text color={'green'}>{successMessage}</Text>
+          }
+        </ModalFooter>
+      </ModalContent>
+    </Modal >
+  )
 }
 
 Profile.getLayout = page => <Layout>{page}</Layout>;
